882544414646d42d4bec0c843ac5699a379755d6
jonreesman <jreesman1@mac.com>
2022-06-01 15:09:42 -0500

    Cleaned up data.

--- components/Chart.js
+++ components/Chart.js
@@ -2,51 +2,6 @@ import React from 'react';
 import {LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer} from 'recharts'
 import styles from '../styles/Chart.module.css'
 
-const data = [
-    {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
-
 const merge = (sentimentHistory, quoteHistory) => {
   let result = [];
   if (quoteHistory.length === 0 || quoteHistory === undefined) {
@@ -90,32 +45,6 @@ const normalizeXAxis = (dataSet) => {
     item.x = (item.TimeStamp - min) / (max-min) * 100
   })
   console.log(dataSet)
-  return dataSet
-}
-
-const setTimeLabels = (dataSet, interval) => {
-  var intervalOptions = new Intl.DateTimeFormat('en-US', {hour: 'numeric'})
-  switch (interval) {
-    case "day":
-      intervalOptions = new Intl.DateTimeFormat('en-US', {hour: 'numeric'})
-      break
-    case "week":
-      intervalOptions = new Intl.DateTimeFormat('en-US', {day: "numeric", hour: 'numeric'})
-      break
-    case "month":
-      intervalOptions = new Intl.DateTimeFormat('en-US', {day: "numeric", hour: 'numeric'})
-      break
-    case "2month":
-      intervalOptions = new Intl.DateTimeFormat('en-US', {month: "numeric", day: "numeric"})
-      break
-    default:
-      intervalOptions = new Intl.DateTimeFormat('en-US', {hour: 'numeric'})
-
-  }
-  
-  dataSet.map(item => {
-    item.TimeString = intervalOptions.format(new Date(item.TimeStamp * 1000))
-  })
   return dataSet
 }
 
