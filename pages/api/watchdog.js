b7893fafd4fb508e3067b50b473f0fc90536e9fa
jonreesman <jreesman1@mac.com>
2022-06-09 13:29:47 -0500

    Changed IP for API.

--- pages/api/watchdog.js
+++ pages/api/watchdog.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://54.163.14.12:8080/api',
+    baseURL: 'http://35.172.216.42:8090/api',
     withCredentials: false,
 });
