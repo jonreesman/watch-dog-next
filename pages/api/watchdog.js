284d207030d0d3c279cce15dc857650d52567fc6
jonreesman <jreesman1@mac.com>
2022-06-09 14:33:46 -0500

    Changed API URL one more time...

--- pages/api/watchdog.js
+++ pages/api/watchdog.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://35.172.216.42:8090/api',
+    baseURL: 'https://wdk.project-archon.com:8090/api',
     withCredentials: false,
 });
