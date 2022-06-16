baaa7cb1531bb808f639b653d2c45bbcb126c67f
jonreesman <jreesman1@mac.com>
2022-06-16 06:44:36 -0500

    Replaced div with Mantine container

--- components/Tweet.js
+++ components/Tweet.js
@@ -1,18 +1,19 @@
 import React from 'react'
-import { useMantineTheme, Card, Text } from '@mantine/core';
+import { useMantineTheme, Container, Card, Text } from '@mantine/core';
 import styles from '../styles/Tweet.module.css'
 
 const Tweet = ({tweet}) => {
   const theme = useMantineTheme();
   return (
-    <div>
+    <Container size={250} px="xs">
       <a href={tweet.PermanentURL} className={styles.link}>
         <Card shadow="sm" p="lg" style={{ backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.colors.blue[4]}}>
-          <Text>{tweet.Expression}</Text>
-          <Text>Sentiment Score: {tweet.Polarity}</Text>
+          <Text size="sm">{tweet.Expression}</Text>
+          <Text size="xs">Likes: {tweet.Likes} Replies: {tweet.Replies} Retweets {tweet.Retweets}</Text>
+          <Text size="xs">Sentiment Score: {tweet.Polarity}</Text>
         </Card>
       </a>
-    </div>
+    </Container>
   )
 }
 
