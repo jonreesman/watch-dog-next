0c3d1bdd3eaa354dcc339fc3b69b497f5fda832c
jonreesman <jreesman1@mac.com>
2022-06-16 06:45:12 -0500

    Split navbar to own component.

--- pages/_appshell.js
+++ pages/_appshell.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react'
 import Timeframes from './_footer';
-import { AppShell, Avatar, Box, Navbar, Header, Aside, Footer, Center, Container, Group, Loader, Text, Modal, Burger, MediaQuery, useMantineTheme, ScrollArea, Button, Grid } from '@mantine/core';
+import CustomNavBar from './_navbar';
+import { AppShell, Avatar, Box, Header, Group, Text, Modal, Burger, MediaQuery, useMantineTheme } from '@mantine/core';
 import Chart from '../components/Chart'
 import TweetAside from './_aside';
 import AboutModal from '../components/AboutModal';
-import TweetList from '../components/TweetList'
 import useTicker from '../hooks/useTicker';
 
 const MainWindow = ({tickers}) => {
@@ -34,39 +34,8 @@ const MainWindow = ({tickers}) => {
           padding="md"
           navbarOffsetBreakpoint="sm"
           asideOffsetBreakpoint="sm"
-          navbar={
-            <Navbar width={{ sm: 200, lg: 300}} p="md" hiddenBreakpoint="sm" hidden={!opened}>
-              <Navbar.Section>
-              <Center><Text>Stocks and Cryptos</Text></Center>
-              </Navbar.Section>
-              <Navbar.Section grow component={ScrollArea} width={{ sm: 200, lg: 300 }}>
-                <Grid gutter="md" grow>
-                  {tickers.map(ticker => {
-                    return (
-                      <Grid.Col span={4} key={ticker.ID}>
-                        <Button variant="filled" onClick={() => setTicker(ticker.Id, )}>
-                          {ticker.Name}
-                        </Button>
-                      </Grid.Col>
-                    )
-                  })}
-                </Grid>
-              </Navbar.Section>
-              <Navbar.Section>
-                <Box sx={{ paddingTop: theme.spacing.sm,
-                            borderTop: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
-                            }`,
-                }}>
-                  <Center>
-                    <Button onClick={() => setModalOpened(true)}>About</Button>
-                  </Center>
-                </Box>
-              </Navbar.Section>
-            </Navbar>
-          }
-          aside={
-            <TweetAside statement_history={ticker_result.statement_history}/>
-          }
+          navbar={<CustomNavBar opened={opened} tickers={tickers} setTicker={setTicker} setModalOpened={setModalOpened} />}
+          aside={<TweetAside statement_history={ticker_result.statement_history}/>}
           header={
             <Header height={70} p="md" style={{ display: 'flex', justifyContent: 'space-between' }}>
               <Group style={{ float: 'left'}}>
